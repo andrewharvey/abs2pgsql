4fcb1577563ccadd8a295f368aeee92ce770538c
Andrew Harvey <andrew.harvey4@gmail.com>
2012-06-23 11:15:40 +1000

    clean up

--- 2011-census/src/02-create-tables.sh
+++ 2011-census/src/02-create-tables.sh
@@ -6,6 +6,6 @@ for profile in $* ; do
   echo "Creating tables for $profile..."
   for structure in ${h[$profile]} ; do
     echo "  ...Structure $structure"
-    cat src/02-$profile-create-schema-template.sql | sed s/{structure}/$structure/g | psql -f - ; \
+    cat src/02-$profile-create-schema-template.sql | sed s/{structure}/$structure/g | psql -f -
   done
 done
--- 2011-census/src/05-load.sh
+++ 2011-census/src/05-load.sh
@@ -6,6 +6,6 @@ for profile in $* ; do
   echo "Loading data for $profile..."
   for structure in ${h[$profile]} ; do
     echo "  ...Structure $structure"
-    src/05-load.pl $structure $profile < load-template ; \
+    src/05-load.pl $structure $profile < load-template
   done
 done
