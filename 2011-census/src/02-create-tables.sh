606793cb930bbc05f23f4d38f39081af8c5a21e1
Andrew Harvey <andrew.harvey4@gmail.com>
2012-06-25 22:12:51 +1000

    since CSV data for SA1 uses the 7-digit code, and our ASGS schema uses the 11-digit code as the PK, we need to add a special case for SA1 to reference the 7-digit code

--- 2011-census/src/02-create-tables.sh
+++ 2011-census/src/02-create-tables.sh
@@ -6,6 +6,13 @@ for profile in $* ; do
   echo "Creating tables for $profile..."
   for structure in ${h[$profile]} ; do
     echo "  ...Structure $structure"
-    cat src/02-$profile-create-schema-template.sql | sed s/{structure}/$structure/g | psql -f -
+    if [ $structure == 'sa1' ] ; then
+      cat src/02-$profile-create-schema-template.sql | sed s/{structure}/$structure/g | \
+      sed "s/sa1_code/sa1_code_7digit/g" | \
+      sed "s/REFERENCES asgs_2011.sa1,/REFERENCES asgs_2011.sa1 (code_7digit),/g" | \
+      psql -f -
+    else
+      cat src/02-$profile-create-schema-template.sql | sed s/{structure}/$structure/g | psql -f -
+    fi
   done
 done
