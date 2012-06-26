afd6ef32d464a252d8e19c23aff7621b15969133
Andrew Harvey <andrew.harvey4@gmail.com>
2012-06-26 19:30:13 +1000

    add global-vars.sh file

---
+++ 2011-census/src/global-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# details of which products are avaliable for which geographic areas
+# was obtained from http://www.abs.gov.au/ausstats/abs@.nsf/lookup/2011.0.55.001Main%20Features1202011
+declare -A h
+h=( [bcp]="sa1 gccsa sua sos sosr ucl ra sla lga ssc poa ced sed" \
+    [pep]="aust ste sa4 sa3 sa2 sa1 gccsa sua sos sosr ucl ra sla lga ssc poa ced sed" \
+    [ip]="aust ste sa4 sa3 sa2 gccsa ireg iare iloc ra sla lga" \
+    [tsp]="aust ste sa4 sa3 sa2 gccsa sla lga" \
+    [erp]="aust ste sa4 sa3 sa2 gccsa sla lga" \
+    [wpp]="aust ste sa4 sa3 sa2 gccsa lga" \
+    [xcp]="aust ste sa4 sa3 sa2 gccsa sua sla lga" \
+    [seifa]="ste sa4 sa3 sa2 sa1 sla lga ssc poa ced" )
