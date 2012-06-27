7e3be0296ab8a41cc20ca4389e7a06ef78d6d74d
Andrew Harvey <andrew.harvey4@gmail.com>
2012-06-27 20:07:31 +1000

    Don't create or load data for geographic areas not yet supported by asgs2pgsql. Closes issue #7.

--- 2011-census/src/global-vars.sh
+++ 2011-census/src/global-vars.sh
@@ -3,11 +3,11 @@
 # details of which products are avaliable for which geographic areas
 # was obtained from http://www.abs.gov.au/ausstats/abs@.nsf/lookup/2011.0.55.001Main%20Features1202011
 declare -A h
-h=( [bcp]="aust ste sa4 sa3 sa2 sa1 gccsa sua sos sosr ucl ra sla lga ssc poa ced sed" \
-    [pep]="aust ste sa4 sa3 sa2 sa1 gccsa sua sos sosr ucl ra sla lga ssc poa ced sed" \
-    [ip]="aust ste sa4 sa3 sa2 gccsa ireg iare iloc ra sla lga" \
-    [tsp]="aust ste sa4 sa3 sa2 gccsa sla lga" \
-    [erp]="aust ste sa4 sa3 sa2 gccsa sla lga" \
+h=( [bcp]="aust ste sa4 sa3 sa2 sa1 gccsa lga ssc poa ced sed" \
+    [pep]="aust ste sa4 sa3 sa2 sa1 gccsa lga ssc poa ced sed" \
+    [ip]="aust ste sa4 sa3 sa2 gccsa ireg iare iloc lga" \
+    [tsp]="aust ste sa4 sa3 sa2 gccsa lga" \
+    [erp]="aust ste sa4 sa3 sa2 gccsa lga" \
     [wpp]="aust ste sa4 sa3 sa2 gccsa lga" \
-    [xcp]="aust ste sa4 sa3 sa2 gccsa sua sla lga" \
-    [seifa]="ste sa4 sa3 sa2 sa1 sla lga ssc poa ced" )
+    [xcp]="aust ste sa4 sa3 sa2 gccsa lga" \
+    [seifa]="ste sa4 sa3 sa2 sa1 lga ssc poa ced" )
