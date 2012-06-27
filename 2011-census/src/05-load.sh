ba3488e77439d80a950a959ce93226c716791eeb
Andrew Harvey <andrew.harvey4@gmail.com>
2012-06-27 21:04:43 +1000

    ANALYZE tables once we have COPYed the data into them

--- 2011-census/src/05-load.sh
+++ 2011-census/src/05-load.sh
@@ -6,6 +6,9 @@ for profile in $* ; do
   echo "Loading data for $profile..."
   for structure in ${h[$profile]} ; do
     echo "  ...Structure $structure"
+    # load the data
     src/05-load.pl $structure $profile < load-template
+    # run ANALYZE on those tables which we added data to as per advice at http://www.postgresql.org/docs/current/static/populate.html#POPULATE-ANALYZE
+    cat load-template | cut -d' ' -f3 | sort | uniq | grep "^$profile" | sed "s/$/_$structure/" | sed "s/^/ANALYZE census_2011./" | sed "s/$/;/" | psql -f -
   done
 done
