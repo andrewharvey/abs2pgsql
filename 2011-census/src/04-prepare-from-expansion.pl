8eb36a3da68d7430bff808e378a6ed6a3a4a7761
Andrew Harvey <andrew.harvey4@gmail.com>
2013-09-18 22:51:39 +1000

    fix multi value inserts

--- 2011-census/src/04-prepare-from-expansion.pl
+++ 2011-census/src/04-prepare-from-expansion.pl
@@ -84,7 +84,7 @@ for my $line (<STDIN>) {
       my ($dataset_num, $insert_value_order, $dst_table) = ($1, $2, $3);
       
       # convert string list representation into a real list
-      my @insert_values = split(/\t/, $insert_value_order);
+      my @insert_values = split(/,/, $insert_value_order);
 
       # trim whitespace from each value in the insert list
       map { s/^\s*//g; s/\s*$//g } @insert_values;
