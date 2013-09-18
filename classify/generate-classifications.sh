ba2b3a4a40dd99256371d074f40f61e1c9d25fda
Andrew Harvey <andrew.harvey4@gmail.com>
2013-09-18 22:53:54 +1000

    make the classify scripts more generic

--- classify/generate-classifications.sh
+++ classify/generate-classifications.sh
@@ -9,14 +9,13 @@ cp $cwd/quantile-classification-template.sql $sql_template
 
 num_classes=$1
 classify_column=$2
-structure=$3
-classify_stat_table=$4
+classify_stat_table=$3
 
 sed -i "s/\$classify_column/$classify_column/g" $sql_template
 sed -i "s/\$num_classes/$num_classes/g" $sql_template
-sed -i "s/\$classify_geom_table/asgs_2011.${structure}_pyramid/g" $sql_template
-sed -i "s/\$classify_stat_table/${classify_stat_table}_${structure}/g" $sql_template
+sed -i "s/\$classify_stat_table/${classify_stat_table}/g" $sql_template
 
 mkdir -p classifications/quantile
-psql --no-align --field-separator=',' -f $sql_template | head  --lines=-1 > classifications/quantile/${classify_stat_table}_${structure}.csv
+psql --no-align --field-separator=',' -f $sql_template | head  --lines=-1 > \
+    classifications/quantile/${classify_stat_table}.${classify_column}.csv
 rm $sql_template
--- classify/quantile-classification-template.sql
+++ classify/quantile-classification-template.sql
@@ -8,11 +8,7 @@ FROM
       $classify_column as classify_column,
       ntile($num_classes) OVER (ORDER BY ($classify_column)) AS ntile
     FROM
-      (
-        SELECT stat.*, geom.area
-        FROM $classify_stat_table stat
-        NATURAL JOIN $classify_geom_table geom
-      ) stats_with_area
+      $classify_stat_table
   ) single_class
 GROUP BY class
 ORDER BY class;
