64a3bfb57b00ad05a57517af05fb733f425d7215
Andrew Harvey <andrew.harvey4@gmail.com>
2017-01-04 20:52:26 +1100

    add script to extract online datapack downloads

---
+++ 2011-census/src/00-extract-datapacks-online.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+for structure in BCP IP TSP PEP XCP WPP ERP; do
+    unzip DataPacksOnline/2011_${structure}_ALL_for_AUST_sequential-header.zip "2011 Census ${structure} All Geographies for AUST/*" -d DataPacks/
+done
