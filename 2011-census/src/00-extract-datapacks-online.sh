275d9935a8193b5a43f3df730d488a29f3e250f7
Andrew Harvey <andrew.harvey4@gmail.com>
2017-01-04 20:59:37 +1100

    also extract metadata

--- 2011-census/src/00-extract-datapacks-online.sh
+++ 2011-census/src/00-extract-datapacks-online.sh
@@ -2,4 +2,5 @@
 
 for structure in BCP IP TSP PEP XCP WPP ERP; do
     unzip DataPacksOnline/2011_${structure}_ALL_for_AUST_sequential-header.zip "2011 Census ${structure} All Geographies for AUST/*" -d DataPacks/
+    unzip -u DataPacksOnline/2011_${structure}_ALL_for_AUST_sequential-header.zip "Metadata/*"
 done
